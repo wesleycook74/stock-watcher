32822b28eda4bce5ebc6f64d78d6791e4c19acea
Wesley Cook <wesleycook74@gmail.com>
2022-02-01 23:32:39 -0500

    Fix unused import

--- src/main/java/com/wcook/service/stock/watcher/service/YahooFinanceStockPriceService.java
+++ src/main/java/com/wcook/service/stock/watcher/service/YahooFinanceStockPriceService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
-import yahoofinance.quotes.stock.StockQuote;
 
 import java.io.IOException;
 import java.util.List;
